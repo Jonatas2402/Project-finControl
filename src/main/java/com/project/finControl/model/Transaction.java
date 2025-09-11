7df45bea31a2bc249d1b714e1d1a0502c842364d
jonatas <jonatas_dev@hotmail.com>
2025-09-11 20:58:35 -0300

    Inicio do projeto

---
+++ src/main/java/com/project/finControl/model/Transaction.java
@@ -0,0 +1,13 @@
+package com.project.finControl.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "transaction")
+@Data
+public class Transaction {
+
+
+}
---
+++ src/main/java/com/project/finControl/model/User.java
@@ -0,0 +1,11 @@
+package com.project.finControl.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+}
