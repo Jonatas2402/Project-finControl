e4c40357f19632b9c38ff1e32ad0fe16cd472d44
jonatas <jonatas_dev@hotmail.com>
2025-09-13 08:17:01 -0300

    TODO: Cria UsersService e inicia salvando um usuário.

---
+++ src/main/java/com/project/finControl/service/UsersService.java
@@ -0,0 +1,17 @@
+package com.project.finControl.service;
+
+import com.project.finControl.model.Users;
+import com.project.finControl.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsersService {
+
+    private final UsersRepository repository;
+
+    public Users save(Users users){
+        return repository.save(users);
+    }
+}
